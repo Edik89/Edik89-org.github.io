686039f27a4379adcec7689ae5b4d72d12b6d65d
Edik89 <bereseduard@gmail.com>
2017-03-16 20:30:15 +0200

    gh_pages

--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -3,11 +3,51 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './components/Menu/App';
 
+
+function checkForRedirect(nextState, replace) {
+  const location = nextState.location;
+  if (location.query.redirect === 'true') {
+    parseRedirectQuery(location.query, replace);
+  } else if (location.pathname.split('/')[1] === gitHubRepoName) {
+    redirectToDomain();
+  }
+}
+
+function parseRedirectQuery(query, replace) {
+  let redirectTo = {};
+
+  if (typeof query.pathname === 'string' && query.pathname !== '') {
+    redirectTo.pathname = query.pathname;
+  }
+
+  if (typeof query.query === 'string' && query.query !== '') {
+    let queryObject = {};
+    query.query.split('&').map(q => q.split('=')).forEach(arr => {
+      queryObject[arr[0]] = arr.slice(1).join('=');
+    });
+    redirectTo.query = queryObject;
+  }
+
+  if (typeof query.hash === 'string' && query.hash !== '') {
+    redirectTo.hash = `#${query.hash}`;
+  }
+
+  replace(redirectTo);
+}
+
+const gitHubRepoName = 'react-redux';
+// The domain for your site
+// SET THIS: e.g. http://subdomain.example.tld, or http://www.example.tld
+const domain = 'https://edik89.github.io/react-redux/';
+function redirectToDomain() {
+  window.location.replace(domain);
+}
+
 const routes = ({ store, history }) => {
   return (
     <Provider store={store}>
       <Router history={history}>
-        <Route component={App}/>
+        <Route onEnter={checkForRedirect} component={App}/>
       </Router>
     </Provider>
   );
