1a648e14fa8c4eaef234f6ac9dfb0a5cfa58e570
Edik89 <bereseduard@gmail.com>
2017-03-16 20:24:39 +0200

    gh_pages

--- frontend/js/components/Menu/App.js
+++ frontend/js/components/Menu/App.js
@@ -8,6 +8,47 @@ import Cart from '../Card/Cart';
 
 import './styles/index.scss';
 
+function checkForRedirect(nextState, replace) {
+  const location = nextState.location;
+  if (location.query.redirect === 'true') {
+    parseRedirectQuery(location.query, replace);
+  } else if (location.pathname.split('/')[1] === gitHubRepoName) {
+    redirectToDomain();
+  }
+}
+
+function parseRedirectQuery(query, replace) {
+  let redirectTo = {};
+
+  if (typeof query.pathname === 'string' && query.pathname !== '') {
+    redirectTo.pathname = query.pathname;
+  }
+
+  if (typeof query.query === 'string' && query.query !== '') {
+    let queryObject = {};
+    query.query.split('&').map(q => q.split('=')).forEach(arr => {
+      queryObject[arr[0]] = arr.slice(1).join('=');
+    });
+    redirectTo.query = queryObject;
+  }
+
+  if (typeof query.hash === 'string' && query.hash !== '') {
+    redirectTo.hash = `#${query.hash}`;
+  }
+
+  replace(redirectTo);
+}
+
+const gitHubRepoName = 'react-redux';
+// The domain for your site
+// SET THIS: e.g. http://subdomain.example.tld, or http://www.example.tld
+const domain = 'https://edik89.github.io/react-redux/';
+function redirectToDomain() {
+  window.location.replace(domain);
+}
+
+
+
 class App extends Component {
 
  static propTypes = {
@@ -23,7 +64,7 @@ class App extends Component {
         <div className="container">
           <TopMenu {...this.props} />
           <div className="content">
-            <Route exact path="/" render={()=> <Cart props={newArr} {...this.props} /> } />
+            <Route exact path="/" onEnter={checkForRedirect} render={()=> <Cart props={newArr} {...this.props} /> } />
             <Route path="/tablets" render={()=> <Cart props={this.props.tablets} {...this.props} /> } />
             <Route path="/laptops" render={()=> <Cart props={this.props.laptops} {...this.props} /> } />
             <Route path="/phones" render={()=> <Cart props={this.props.phones} {...this.props} /> } />
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -11,47 +11,6 @@ import Root from './routes';
 const store = configureStore();
 const history = syncHistoryWithStore(createHistory(), store);
 
-
-function checkForRedirect(nextState, replace) {
-  const location = nextState.location;
-  if (location.query.redirect === 'true') {
-    parseRedirectQuery(location.query, replace);
-  } else if (location.pathname.split('/')[1] === gitHubRepoName) {
-    redirectToDomain();
-  }
-}
-
-function parseRedirectQuery(query, replace) {
-  let redirectTo = {};
-
-  if (typeof query.pathname === 'string' && query.pathname !== '') {
-    redirectTo.pathname = query.pathname;
-  }
-
-  if (typeof query.query === 'string' && query.query !== '') {
-    let queryObject = {};
-    query.query.split('&').map(q => q.split('=')).forEach(arr => {
-      queryObject[arr[0]] = arr.slice(1).join('=');
-    });
-    redirectTo.query = queryObject;
-  }
-
-  if (typeof query.hash === 'string' && query.hash !== '') {
-    redirectTo.hash = `#${query.hash}`;
-  }
-
-  replace(redirectTo);
-}
-
-const gitHubRepoName = 'react-redux';
-// The domain for your site
-// SET THIS: e.g. http://subdomain.example.tld, or http://www.example.tld
-const domain = 'https://edik89.github.io/react-redux/';
-function redirectToDomain() {
-  window.location.replace(domain);
-}
-
-
 renderWithHotReload(Root);
 
 if (module.hot) {
